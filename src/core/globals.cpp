1932c01106855ab44d349a13db5df0737c5320ce
Michael Koval <mkoval@cs.cmu.edu>
2015-11-15 18:43:02 -0500

    Updated examples to use the despot namespace.

--- src/core/globals.cpp
+++ src/core/globals.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 using namespace despot;
 
+namespace despot {
 namespace Globals {
 Config config;
 ExecTracker tracker;
@@ -10,4 +11,5 @@ const double INF = 1e8;
 const double TINY = 1e-8;
 const double POS_INFTY = numeric_limits<double>::max();
 const double NEG_INFTY = -POS_INFTY;
-} // namespace
+} // namespace Globals
+} // namespace despot
